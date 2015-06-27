52a8584c70b36746f16d345196d8133d35c106fe
Walter Llop Masiá <wllop@esat.es>
2015-06-27 13:18:18 +0200

    Adaptación Licencias

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # snapweb
-Snapweb Project to freezer your site!!
+# Snapweb Project to freezer your site!!
+SnapWeb permite la monitorización de directorios impidiendo, en su caso, cambios en el mismo. Además, dispone de distintos tipos de bloqueos, permitiendo incluso, el análisis en tiempo real de malware.
+
+USO:
--- jack.sh
+++ jack.sh
@@ -1,5 +1,19 @@
-#!/bin/bash
-#Copyright 2015 Walter Llop Masia @wllop
+#/bin/bash
+#
+# Copyright 2015 Walter LLop Masiá @wllop
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 #Por lo visto, al menos en debian, cuando el script lo llama incrond, lo hace como comando $_ --> /sbin/start-stop-daemon
 #Recibido de incron!
 #$1 --> Ruta
