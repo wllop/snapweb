2ba3b21c904c9d0b9e66eb2df5c8e62f46b51c7e
Walter Llop Masiá <wllop@esat.es>
2015-07-25 00:42:47 +0200

    Mostrar fichero que ha sido modificado y marcado como posible malware.

--- jack.sh
+++ jack.sh
@@ -184,7 +184,7 @@ elif [ "$3" = "IN_MOVED_TO" ]; then #Nueva fichero eliminado!
       0)total=($(check $1/$2))
         echo ${total[0]} >>/usr/local/snapweb/msg.log
         if [ ${total[0]} -gt 5 ]; then #Controlar
-          echo "Se ha creado un nuevo fichero con código sospecho:${total[*]}"|mail -s "SNAPWEB: Código sospechoso " $mail_destino
+          echo "Se ha creado un nuevo fichero $1/$2 con código sospecho:${total[*]}"|mail -s "SNAPWEB: Código sospechoso " $mail_destino
         fi
         cp -fpr $1/$2 $base/$subdir
         ;;    
