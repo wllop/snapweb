06f2278e3aaf01351f1f3ae68ec201533a1bac5d
Walter Llop Masiá <wllop@esat.es>
2015-08-08 17:36:35 +0200

    Nuevo script para actualizar snapweb

---
+++ update.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+#
+# Copyright 2015 Walter LLop Masiá @wllop
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+fatal(){ echo ; echo "Error: $@" >&2;${E:+exit $E};}
+installed(){ type -p "$1" 2>/dev/null >/dev/null;}
+[ -d .git ] && git pull || E1 fatal "Para poder lanzar la actualización de SnapWeb a través de update.sh, debe estar ubicado en el directorio del repositorio git." 
+if ruta=$(type -p snapweb.sh); then 
+  if ! diff -rq ./snapweb.sh $ruta ;then
+   cp -f ./snapweb.sh $ruta
+  fi
+else
+   echo "Snapweb no está incluido en la variable PATH."
+   read -n 1 -p "¿Desea incluirlo en /usr/local/sbin (S/n)" res
+   if [ "res" != "n" ]; then
+    cp -f ./snapweb.sh /usr/local/sbin/snapweb.sh
+   fi
+fi
+if ! diff -rq ./jack.sh /usr/local/snapweb/jack.sh ;then
+   cp -f ./jack.sh /usr/local/snapweb/jack.sh
+fi
